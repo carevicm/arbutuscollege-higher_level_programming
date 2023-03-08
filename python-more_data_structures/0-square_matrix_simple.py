c60c4e6ad7cbdaa48c5d7466fb15e3de5dd99e5d
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-08 13:39:01 -0800

    zero commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-
-return ([list(map(lambda x: x * x, row)) for row in matrix])
+    if matrix:
+        return [list(map(lambda j: j**2, i)) for i in matrix]
