2d646ee58579ff51c4d0e5155e695e80af23f495
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-08 14:27:08 -0800

    fifth commit

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines a function that returns the number of keys in a dictionary."""
+
 
 def number_keys(my_dict):
-    """Returns the number of keys in a dictionary."""
     return len(my_dict.keys())
