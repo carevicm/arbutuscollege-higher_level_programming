0bfbdb5da3cb694a35d6888125639d75cf674f85
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-07 17:39:21 -0800

    fifth commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
+    new_str = ''
     for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
-    return new
+        if i != 'C' and i != 'c':
+            new_str += i
+
+    return new_str
