9e4923951c8ce5e60a90335faaed8ef6655fd1c0
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-09 14:34:33 -0800

    twelfth commit

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    roman_dict = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    result = 0
+    prev = 0
+    for c in roman_string:
+        value = roman_dict.get(c, 0)
+        if value == 0:
+            return 0
+        if prev < value:
+            result -= 2 * prev
+        result += value
+        prev = value
+    return result
