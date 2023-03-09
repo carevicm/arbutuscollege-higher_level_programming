d86279ce72f530d0e25cd032d9d41e3d43644569
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-09 14:12:38 -0800

    tenth commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,12 @@
 
 
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if isinstance(value, int) and value > best_value:
+            best_key = key
+            best_value = value
+    return best_key
