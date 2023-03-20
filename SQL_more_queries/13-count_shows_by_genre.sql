c74278272e759f64aaaaea00dfcc202f6503406d
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-20 16:47:39 -0700

    thirteenth commit

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,8 @@
 -- script that list all genres from tv show and display number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
-FROM tv_show_genres
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
-ORDER BY number_shows DESC;
+SELECT g.`name` AS `genre`,
+       COUNT(*) AS `number_of_shows`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS t
+       ON g.`id` = t.`genre_id`
+ GROUP BY g.`name`
+ ORDER BY `number_of_shows` DESC;
