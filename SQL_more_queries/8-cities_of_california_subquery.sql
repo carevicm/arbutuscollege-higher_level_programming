c2d27e4f8e63c57cdd6e96e13ffc36db15c26cea
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-03-20 15:33:21 -0700

    eight commit

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,6 @@
 -- script lists all cities of california
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name='California') ORDER BY id;
+SELECT cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
