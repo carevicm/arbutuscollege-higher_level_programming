022dfd6a671e137106b1f09d2782bf80f932cd0a
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-04-14 12:12:02 -0700

    second commit

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if two objects are the same class
+    """
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be a type")
+    return (type(obj) is a_class)
