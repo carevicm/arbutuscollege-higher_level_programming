d92dbc0bbfe383c5ac4cc7d420c756687e24bd85
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-04-14 12:24:18 -0700

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-       issubclass(a_class, obj.__class__) is False:
-        return True
 
+
+def inherits_from(obj, a_class):
+
+    
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
     return False
