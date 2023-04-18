cc44416e9c76dd90b11b7b654e6afd5ef8e940cc
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-04-18 10:52:37 -0700

    Update 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""
-This file contains functiom
-that returns the number of lines
-of a text file
-"""
+# 3-write_file.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a file-writing function."""
 
 
-def number_of_lines(filename=""):
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
     """
-    function returns number of lines
-    """
-    i = 0
-    with open(filename, encoding="utf-8") as myFile:
-        for line in myFile:
-            i += 1
-    return (i)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
