6ab31e93cce65aa795e132cec2d66b21ecbdca68
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-04-19 10:16:11 -0700

    third commit

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This time the script is safe from
+MySQL injections!
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    with db.cursor() as cur:
+        cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+
+        rows = cur.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
