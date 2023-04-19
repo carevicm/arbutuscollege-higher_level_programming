81c0b35b93d2a733c1b27293bc67de01d42cfd84
Milan Carevic <96025559+carevicm@users.noreply.github.com>
2023-04-19 09:59:26 -0700

    zero commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,10 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+     """
+    Access to the database and get the states
+    from the database.
+    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
